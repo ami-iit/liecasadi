d657ad336d1d15cce638edbc49a979190a9e46dd
Giuseppe <gl.giuseppelerario@gmail.com>
2022-01-24 17:30:32 +0100

    Using dataclass + adding some methods

--- src/liecasadi/dualquaternion.py
+++ src/liecasadi/dualquaternion.py
@@ -2,39 +2,72 @@
 # This software may be modified and distributed under the terms of the
 # GNU Lesser General Public License v2.1 or any later version.
 
+from curses.ascii import SO
+import dataclasses
+from attr import field
 import casadi as cs
 
-from liecasadi import Quaternion
-from liecasadi.hints import Vector
+from liecasadi import Quaternion, SO3, SE3
+from liecasadi.hints import Matrix, Scalar, Vector
 
 
+@dataclasses.dataclass
 class DualQuaternion:
-    def __init__(self, qr, qd):
-        self.qr = Quaternion(qr)
-        self.qd = Quaternion(qd)
+    qr: Vector
+    qd: Vector
+    Qr: Quaternion = field(init=False)
+    Qd: Quaternion = field(init=False)
+
+    def __post_init__(self):
+        self.Qr = Quaternion(self.qr)
+        self.Qd = Quaternion(self.qd)
 
     def __repr__(self) -> str:
-        return "q_r: {} \nq_d: {}".format(str(self.qr), str(self.qd))
+        return f"Rotation quaternion: {self.Qr.xyzw} \nTranslation quaternion: {self.Qd.coeffs()}"
 
-    def __mul__(self, other):
-        qr = self.qr * other.qr
-        qd = self.qr * other.qd + self.qd * other.qr
-        return DualQuaternion(qr=qr.q, qd=qd.q)
+    def __mul__(self, other: "DualQuaternion") -> "DualQuaternion":
+        qr = self.Qr * other.Qr
+        qd = self.Qr * other.Qd + self.Qd * other.Qr
+        return DualQuaternion(qr=qr.coeffs(), qd=qd.coeffs())
+
+    def __rmul__(self, other: Scalar) -> "DualQuaternion":
+        """Multiplication with a scalar
+
+        Returns:
+            Dual Quaternion
+        """
+        return DualQuaternion(qr=other * self.qr, qd=other * self.qd)
 
     @staticmethod
-    def from_quaternion_and_translation(quat, transl):
+    def from_quaternion_and_translation(
+        quat: Vector, transl: Vector
+    ) -> "DualQuaternion":
         t = Quaternion(cs.vertcat(transl, 0))
         r = Quaternion(quat)
-        qd = 0.5 * (t * r).q
-        return DualQuaternion(qr=r.q, qd=qd)
+        qd = 0.5 * (t * r).coeffs()
+        return DualQuaternion(qr=r.coeffs(), qd=qd)
 
-    def inverse(self):
-        qr_inv = self.qr.inverse()
-        qd = -qr_inv * self.qd * qr_inv
+    @staticmethod
+    def from_matrix(m: Matrix) -> "DualQuaternion":
+        se3 = SE3.from_matrix(m)
+        t = se3.rotation().as_quat()
+        r = Quaternion(cs.vertcat(se3.translation(), 0))
+        qd = 0.5 * (t * r).coeffs()
+        return DualQuaternion(qr=r.coeffs(), qd=qd)
+
+    def translation(self) -> Vector:
+        return 2.0 * (self.Qd * self.Qr.conjugate()).coeff()
+
+    def rotation(self) -> SO3:
+        return SO3(xyzw=self.Qr.coeffs())
+
+    def inverse(self) -> "DualQuaternion":
+        qr_inv = self.Qr.conjugate()
+        qd = -qr_inv * self.Qd * qr_inv
         return DualQuaternion(qr=qr_inv, qd=qd)
 
     def translation(self):
-        return 2 * (self.qd * self.qr.inverse()).q
+        return 2 * (self.Qd * self.Qr.conjugate()).coeffs()
 
 
 if __name__ == "__main__":
@@ -44,7 +77,6 @@ if __name__ == "__main__":
     quat = quat / np.linalg.norm(quat)
 
     trans = np.random.randn(3) * 4
-    print(trans)
     dq = DualQuaternion.from_quaternion_and_translation(quat, trans)
 
     quat2 = np.random.randn(4) * 4
@@ -54,5 +86,7 @@ if __name__ == "__main__":
 
     dq2 = DualQuaternion(quat2, trans2)
 
-    print((dq * dq2).inverse())
+    d3 = DualQuaternion.from_matrix(np.eye(4))
+
+    print((3 * dq2).inverse())
     print(dq.translation())
