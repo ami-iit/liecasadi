d950cfec7b8ec7a83bbf6ed7c08f2119ea0777e2
giulero <gl.giuseppelerario@gmail.com>
2022-07-25 13:00:45 +0200

    Remove integration method

--- src/liecasadi/so3.py
+++ src/liecasadi/so3.py
@@ -114,27 +114,7 @@ class SO3:
         # if d_omega in spatial representation
         A = 0.5 * cs.vertcat(
             cs.horzcat(0, -d_omega[0], -d_omega[1], -d_omega[2]),
-            cs.horzcat(d_omega[0], 0, d_omega[2], -d_omega[1]),
-            cs.horzcat(d_omega[1], -d_omega[2], 0, d_omega[0]),
-            cs.horzcat(d_omega[2], d_omega[1], -d_omega[0], 0),
-        )
-        if representation == "spatial":
-            A[1:, 1:] = A[1:, 1:].T
-        return A
-
-    def add_integrated_velocity(
-        self, d_omega: Vector, representation: str = ["spatial", "body"]
     ):
-        repr_types = ["spatial", "body"]
-        if representation not in repr_types:
-            raise ValueError(f"Invalid representation. Expected are {repr_types}")
-        quat = self.as_quat().coeffs()
-        quat = cs.vertcat(quat[3], quat[0], quat[1], quat[2])
-
-        def _exp(o):
-            A = SO3._quat_dot_map(o, representation)
-            nor = cs.norm_2(o)
-            return cs.cos(nor / 2) * cs.DM.eye(4) + 2 * cs.sin(nor / 2) / nor * A
 
         quat = _exp(d_omega) * quat
         return SO3(xyzw=cs.vertcat(quat[1], quat[2], quat[3], quat[0]))
