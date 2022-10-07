e9d29c115fe5f08d8428e612b0bde812ccf99dee
giulero <gl.giuseppelerario@gmail.com>
2022-10-07 12:35:03 +0200

    Add some documentation

--- src/liecasadi/dualquaternion.py
+++ src/liecasadi/dualquaternion.py
@@ -14,17 +14,26 @@ from liecasadi.hints import Matrix, Scalar, Vector
 
 @dataclasses.dataclass
 class DualQuaternion:
+    """
+    Dual Quaternion class
+    Have a look at the document https://cs.gmu.edu/~jmlien/teaching/cs451/uploads/Main/dual-quaternion.pdf for some intuitions.
+    """
+
     qr: Vector
     qd: Vector
     Qr: Quaternion = field(init=False)
     Qd: Quaternion = field(init=False)
 
     def __post_init__(self):
-        warnings.warn("[DualQuaternion]: This class is under development!")
+        """Build two Quaternion objects from xyzw quaternion and xyz translation"""
         self.Qr = Quaternion(self.qr)
         self.Qd = Quaternion(self.qd)
 
     def __repr__(self) -> str:
+        """
+        Returns:
+            str: when you print the dual quaternion
+        """
         return f"Rotation quaternion: {self.Qr.xyzw} \nTranslation quaternion: {self.Qd.coeffs()}"
 
     def __mul__(self, other: "DualQuaternion") -> "DualQuaternion":
@@ -78,6 +87,15 @@ class DualQuaternion:
     def from_quaternion_and_translation(
         quat: Vector, transl: Vector
     ) -> "DualQuaternion":
+        """Build dual quaternion from a quaternion (xyzw) and a translation vector (xyz)
+
+        Args:
+            quat (Vector): xyzw quaternion vector
+            transl (Vector): xyz translation vector
+
+        Returns:
+            DualQuaternion: a dual quaternion
+        """
         t = Quaternion(cs.vertcat(transl, 0))
         r = Quaternion(quat)
         qd = 0.5 * (t * r).coeffs()
@@ -85,6 +103,14 @@ class DualQuaternion:
 
     @staticmethod
     def from_matrix(m: Matrix) -> "DualQuaternion":
+        """Build dual quaternion from an homogenous matrix
+
+        Args:
+            m (Matrix): homogenous matrix
+
+        Returns:
+            DualQuaternion: a dual quaternion
+        """
         se3 = SE3.from_matrix(m)
         r = se3.rotation().as_quat()
         t = Quaternion(cs.vertcat(se3.translation(), 0))
@@ -92,27 +118,55 @@ class DualQuaternion:
         return DualQuaternion(qr=r.coeffs(), qd=qd)
 
     def translation(self) -> Vector:
+        """
+        Returns:
+            Vector: Translation vector xyz
+        """
         return 2.0 * (self.Qd * self.Qr.conjugate()).coeffs()
 
     def rotation(self) -> SO3:
+        """
+        Returns:
+            SO3: an SO3 object
+        """
         return SO3(xyzw=self.Qr.coeffs())
 
     def inverse(self) -> "DualQuaternion":
+        """
+        Returns:
+            DualQuaternion: a dual quaternion, inverse of the original
+        """
         qr_inv = self.Qr.conjugate()
         qd = -qr_inv * self.Qd * qr_inv
         return DualQuaternion(qr=qr_inv.coeffs(), qd=qd.coeffs())
 
-    def conjugate(self):
+    def conjugate(self) -> "DualQuaternion":
+        """
+        Returns:
+            DualQuaternion: conjugate dual quaternion
+        """
         qr = self.Qr.conjugate()
         qd = self.Qd.conjugate()
         return DualQuaternion(qr=qr.coeffs(), qd=qd.coeffs())
 
-    def as_matrix(self):
+    def as_matrix(self) -> Matrix:
+        """
+        Returns:
+            Matrix: the corresponding homogenous matrix
+        """
         r = self.rotation().as_matrix()
         t = self.translation()
+        return cs.vertcat(
+            cs.horzcat(r, t),
+            cs.horzcat([0, 0, 0, 1]).T,
+        )
 
     @staticmethod
-    def Identity():
+    def Identity() -> "DualQuaternion":
+        """
+        Returns:
+            DualQuaternion: the identity dual quaternion
+        """
         return DualQuaternion(
             qr=SO3.Identity().as_quat().coeffs, qd=Quaternion([0, 0, 0, 0])
         )
