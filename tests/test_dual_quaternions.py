f42f118aaaa30efac9255f40f83a6c40032c134d
giulero <gl.giuseppelerario@gmail.com>
2022-10-07 15:51:12 +0200

    Update readme with example

--- tests/test_dual_quaternions.py
+++ tests/test_dual_quaternions.py
@@ -1,19 +1,18 @@
 import numpy as np
 import pytest
-from dual_quaternions import DualQuaternion as TestDualQuat
-from icecream import ic
 
 from liecasadi import SE3, DualQuaternion
 
-# quat generation
-quat = (np.random.rand(4) - 0.5) * 5
+# orientation quaternion generation
+quat = np.random.randn(4) * 5
 quat = quat / np.linalg.norm(quat)
 
-quat2 = (np.random.rand(4) - 0.5) * 5
+quat2 = np.random.randn(4) * 5
 quat2 = quat2 / np.linalg.norm(quat2)
 
-pos = (np.random.rand(3) - 0.5) * 5
-pos2 = (np.random.rand(3) - 0.5) * 5
+# translation vector generation
+pos = np.random.randn(3) * 5
+pos2 = np.random.randn(3) * 5
 
 
 H1 = SE3.from_position_quaternion(pos, quat).as_matrix()
@@ -24,15 +23,15 @@ dual_q2 = DualQuaternion.from_matrix(H2)
 
 
 def test_concatenation():
-    conc_dual_q = dual_q1 * dual_q2
-    conc_H = H1 @ H2
-    assert conc_dual_q.as_matrix() - conc_H == pytest.approx(0.0, abs=1e-4)
+    concat_dual_q = dual_q1 * dual_q2
+    concat_H = H1 @ H2
+    assert concat_dual_q.as_matrix() - concat_H == pytest.approx(0.0, abs=1e-4)
 
 
 def test_transform_point():
-    point = np.random.rand(4, 1)
+    point = np.random.randn(4, 1) * 5
     point[3] = 1.0
-    assert dual_q1.transform_point(point) - (H1 @ point)[:3] == pytest.approx(
+    assert dual_q1.transform_point(point[:3]) - (H1 @ point)[:3] == pytest.approx(
         0.0, abs=1e-4
     )
 
