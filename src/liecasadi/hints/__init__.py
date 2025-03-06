7e7621443e6ffdd2c92c0853d1dc338f0f3b59de
giulero <gl.giuseppelerario@gmail.com>
2025-03-06 13:11:59 +0100

    Fix  type annotations for Vector, Scalar, and Matrix in hints module

--- src/liecasadi/hints/__init__.py
+++ src/liecasadi/hints/__init__.py
@@ -2,9 +2,8 @@ from typing import Union
 
 import casadi as cs
 import numpy.typing as ntp
-
-Vector = Union[ntp.NDArray, cs.MX]
-Scalar = Union[float, cs.MX]
-Matrix = Union[ntp.NDArray, cs.MX]
+Vector = Union[cs.DM, cs.MX, ntp.NDArray]
+Scalar = Union[cs.DM, cs.MX, float]
+Matrix = Union[cs.DM, cs.MX, ntp.NDArray]
 Angle = Vector
 TangentVector = Vector
